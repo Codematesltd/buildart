a413c8aa5f6851cd4041c2ab90fa91209f891589
Shrinivas-03 <shrinivasnadager03@gmail.com>
2025-05-09 17:24:33 +0530

    security

--- app.py
+++ app.py
@@ -1,32 +1,39 @@
 from flask import Flask, render_template
-from extensions import login_manager
+from config import Config
+from extensions import init_extensions, cache
 from routes.admin import admin_bp
+from flask_minify import minify
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'your-secret-key'
+    app.config.from_object(Config)
     
-    login_manager.init_app(app)
-    app.register_blueprint(admin_bp, url_prefix='/admin')
+    init_extensions(app)
+    minify(app=app)  # Add minify initialization here
     
     # Main routes
     @app.route('/')
+    @cache.cached(timeout=300)
     def index():
         return render_template('index.html')
     
     @app.route('/about')
+    @cache.cached(timeout=300)
     def about():
         return render_template('about_us.html')
     
     @app.route('/awards')
+    @cache.cached(timeout=300)
     def awards():
         return render_template('awards.html')
     
     @app.route('/enquiry')
+    @cache.cached(timeout=300)
     def enquiry():
         return render_template('enquiry.html')
     
     @app.route('/projects')
+    @cache.cached(timeout=300)
     def projects():
         return render_template('project.html')
     
@@ -35,4 +42,4 @@ def create_app():
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, ssl_context='adhoc')
