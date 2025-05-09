aa6c8d936e576215553ac630ac4ef59136437409
Shrinivas-03 <shrinivasnadager03@gmail.com>
2025-05-09 17:07:21 +0530

    initial configs

--- app.py
+++ app.py
@@ -1,13 +1,34 @@
-from flask import Flask
+from flask import Flask, render_template
 from extensions import login_manager
 from routes.admin import admin_bp
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'your-secret-key'  # Change this in production
+    app.config['SECRET_KEY'] = 'your-secret-key'
     
     login_manager.init_app(app)
     app.register_blueprint(admin_bp, url_prefix='/admin')
+    
+    # Main routes
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+    
+    @app.route('/about')
+    def about():
+        return render_template('about_us.html')
+    
+    @app.route('/awards')
+    def awards():
+        return render_template('awards.html')
+    
+    @app.route('/enquiry')
+    def enquiry():
+        return render_template('enquiry.html')
+    
+    @app.route('/projects')
+    def projects():
+        return render_template('project.html')
     
     return app
 
--- config.py
+++ config.py
@@ -6,3 +6,5 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key'
     UPLOAD_FOLDER = os.path.join('static', 'uploads')
+    SUPABASE_URL = os.environ.get('SUPABASE_URL') or 'https://zpnparyxnrtfbxnwriyv.supabase.co'
+    SUPABASE_KEY = os.environ.get('SUPABASE_KEY') or 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpwbnBhcnl4bnJ0ZmJ4bndyaXl2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY3OTAxMTcsImV4cCI6MjA2MjM2NjExN30.mg_g4fw-AiabzKUJnWxU7C10eiCUCQAurygIv5fzWG0'
