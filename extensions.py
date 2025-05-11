f552b633636b699726c52ac088f4ddbcc7c6b47d
Shrinivas-03 <shrinivasnadager03@gmail.com>
2025-05-11 14:26:59 +0530

    fixes

--- extensions.py
+++ extensions.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 from flask_login import LoginManager
 from flask_wtf import CSRFProtect  # Modified import
 from flask_talisman import Talisman
@@ -22,5 +22,3 @@ def init_extensions(app):
     # Enforce HTTPS and set secure headers
     Talisman(app, content_security_policy=None)
 
-=======
->>>>>>> 3edbd9158125721aae2d1d367eb2a8182792acde
